c9c871c19bb09cc4fc38ef7b6b5006ec74445458
sharolin <sharolin33@gmail.com>
2018-10-11 22:34:48 -0600

    nuevo

---
+++ Pagina Web/CarroVirtual/BDDPruebasFuncionales/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BDDPruebasFuncionales")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BDDPruebasFuncionales")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fd12ec0e-fb00-4986-9257-89fa8db99821")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pagina Web/CarroVirtual/BDDPruebasFuncionales/crearCategoriaCorrectamente.feature
@@ -0,0 +1,11 @@
+﻿Feature: crearCategoriaCorrectamente
+	Para evitar errores al crear una categoria
+	Como un usuario del sistema
+	Quiero que al llenar todos los campos de forma correcta se cree la nueva categoria
+
+@mytag
+Scenario: crear Categoria Correctamente
+	Given el nombre de la categoria "Alcazar"
+	And la descripcion "somos el grupo de analisis 1"
+	When cuando presiones el boton de crea la nueva categoria 
+	Then se debe crear la categoria con su nombre y descripcion esto validado con un "si"
